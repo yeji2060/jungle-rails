673fcf4fd7988455f9cc94d3bef07e739a8c06f5
Yeji Ha <yeji206000@gmail.com>
2024-06-03 02:13:06 -0700

    add user authentication feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
 
   private
 
@@ -33,4 +34,17 @@ class ApplicationController < ActionController::Base
       username == ENV["USERNAME"] && password == ENV["PASSWORD"]
     end
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    redirect_to login_path unless logged_in?
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+  
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   namespace :admin do
     get 'categories/index'
     get 'categories/new'
